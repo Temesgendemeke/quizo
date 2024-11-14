d45fc554e17438887af4c6039c86baa42bea0ada
Temesgendemeke <tdemke36@gmail.com>
2024-11-14 20:59:12 +0300

    modfied model

--- quiz/models.py
+++ quiz/models.py
@@ -2,13 +2,14 @@ from sys import maxsize
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.base import Model
+from django.utils.choices import BlankChoiceIterator
 
 # Create your models here.
 
 
 class Subject(models.Model):
     name = models.CharField(max_length=101)
-    image = models.ImageField(upload_to="images", null=True, blank=True)
+    image = models.URLField(max_length=200, null=True)
 
     def __str__(self):
         return f"{self.name}"
@@ -18,7 +19,7 @@ class Question(models.Model):
     question = models.CharField(max_length=101)
     option1 = models.CharField(max_length=100)
     option2 = models.CharField(max_length=100)
-    option3 = models.CharField(max_length=100)
+    option3 = models.CharField(max_length=100, null=True, blank=True)
     option4 = models.CharField(max_length=100)
     answer = models.CharField(max_length=101)
 
